f90decbae2152ea8abd2effef0695dc57c38a524
dejosemario <josemariaodumodu@gmail.com>
2024-04-20 14:33:38 +0100

    feat: create the auth and post routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
-// import * as routes from "./routes";
-// import * as errorMiddleware from "./middlewares/error.js";
+import * as routes from "./routes";
+import * as errorMiddleware from "./middlewares/error.js";
 
 // Create a new express application
 const app = express();
@@ -8,15 +8,17 @@ const app = express();
 // Use the express.json() middleware to parse the request body
 app.use(express.json());
 
+//Routes
+app.use("/api/auth", routes.authRoute);
+app.use("/api/posts", routes.postRoute);
+
 // Error handling middleware
-// app.use(errorHandler);
+app.use(errorHandler);
 app.all("*", (req, res) => {
-  res
-    .status(404)
-    .json({
-      success: "false",
-      message: `Can't find ${req.originalUrl} on this server!`,
-    });
+  res.status(404).json({
+    success: "false",
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
 });
 
 export default app;
