57c3504acb6b59af46fe4e50e904da610aad91a0
dejosemario <josemariaodumodu@gmail.com>
2024-04-20 21:45:03 +0100

    feat: create the auth services

---
+++ src/services/auth.service.js
@@ -0,0 +1,39 @@
+import User from "../models/users.model.js";
+import {
+  hashPassword,
+  comparePassword,
+  generateToken,
+} from "../utils/index.js";
+
+const createUser = async (data) => {
+  const user = await User.findOne({ email: data.email });
+  if (user) {
+    throw new Error("User already exist");
+  }
+  console.log(user);
+  const hashedPassword = await hashPassword(data.password);
+  const newUser = new User({ ...data, password: hashedPassword });
+  await newUser.save();
+  return newUser;
+};
+
+const login = async (email, password) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error("User not found");
+  }
+  const isMatch = await comparePassword(password, user.password);
+  if (!isMatch) {
+    throw new Error("Invalid credentials");
+  }
+
+  //generate the access token
+  const token = generateToken({
+    id: user._id,
+    first_name: user.first_name,
+    email: user.email,
+  });
+
+  return { user, token };
+};
+export { createUser, login };
